4685b0421d2ccc1d7b03b9116c21dd185d6e8d3f
Pavel Markov <pavel.markov@ingrammicro.com>
2020-04-23 15:15:29 +0300

    added gitignore

--- src/main.js
+++ src/main.js
@@ -6,12 +6,9 @@ import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import './style/style.scss'
-import { dom } from '@fortawesome/fontawesome-svg-core'
 import datePicker from 'vue-bootstrap-datetimepicker'
 import 'pc-bootstrap4-datetimepicker/build/css/bootstrap-datetimepicker.css'
 import VueBootstrapTypeahead from 'vue-bootstrap-typeahead'
-
-dom.watch()
 
 Vue.component('vue-bootstrap-typeahead', VueBootstrapTypeahead)
 Vue.use(BootstrapVue)
