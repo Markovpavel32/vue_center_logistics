b019bf193507cb5ae2e29588029b3d7c380110b2
Pavel Markov <pavel.markov@ingrammicro.com>
2020-04-24 11:18:39 +0300

    removed from ApplicationLogAppointment.vue numeration

--- src/components/application_log/ApplicationLogAppointment.vue
+++ src/components/application_log/ApplicationLogAppointment.vue
@@ -24,9 +24,6 @@
         <strong>Загрузка...</strong>
       </div>
     </template>
-    <template v-slot:cell(number)="data">
-      <span>{{ data.index + 1 }}</span>
-    </template>
     <template v-slot:cell(status)="data">
       <span class="badge status_badge"  :class="status_badge(data.value)">{{ data.value }}</span>
     </template>
@@ -66,10 +63,6 @@ export default {
       total_rows: 0,
       per_page: 20,
       fields: [
-        {
-          key: 'number',
-          label: ''
-        },
         {
           key: 'document_date',
           label: 'Дата создания',
